306374351241f6f5a8492f98771af454eb26eb8a
Karen Shea <shea.karene@gmail.com>
2019-04-07 18:39:25 -0400

    successful testing of REST API in React

--- client/src/App.js
+++ client/src/App.js
@@ -8,16 +8,55 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 
-var {User, Course} = require("../../api/models");
+// var {User, Course} = require("../../api/models");
 
 
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      courses: []
+    };
+  } 
+
+
+  getCourses = () => {
+    axios({
+      method: "get",
+      url: "http://localhost:5000/api/courses"
+    }).then(res => {
+      this.setState({courses: JSON.stringify(res.data)})
+    })
+  }
+
+  componentDidMount() {
+    // let courses = axios.get('http://localhost:5000/api/courses').data
+    this.getCourses();
+    // this.setState({courses: JSON.stringify(courses)})
+    console.log(this.state.courses)
+    // .then(function (response){
+    //   console.log(response.data)
+
+    //   this.setState({
+    //     courses: response.data
+    //   })
+    //   // console.log(this.state.courses)
+    // })
+      // .catch(function(error) {
+      //   console.log(error);
+      // });
+  }
+
+
+
   render() {
     return (
-      <BrowserRouter>
-        <Route exact path= '/' render={() => <Course/>}/>
-      </BrowserRouter>
+      <div>
+      <h1>Courses</h1>
+      {this.state.courses}
+      </div>
     );
   }
 }
