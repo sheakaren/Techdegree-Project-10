4aafb0c47a6c1a98a68b7e27092e9547cb09efc9
Karen Shea <shea.karene@gmail.com>
2019-04-12 22:06:36 -0400

    updated the CreateCourse component

--- client/src/components/CreateCourse.js
+++ client/src/components/CreateCourse.js
@@ -27,6 +27,7 @@ class CreateCourse extends Component {
     handleSubmit(e) {
         e.preventDefault();
 
+        const userData = JSON.parse(localStorage.getItem('userData'));
         
         axios({
             method: "POST",
@@ -35,7 +36,8 @@ class CreateCourse extends Component {
                 title: this.state.title,
                 description: this.state.description,
                 estimatedTime: this.state.estimatedTime,
-                materialsNeeded: this.state.materialsNeeded
+                materialsNeeded: this.state.materialsNeeded,
+                user: userData._id
             },
             auth: {
                 username: localStorage.getItem('username'),
@@ -47,43 +49,44 @@ class CreateCourse extends Component {
     } // end handleSubmit()
 
     render() {
-              return (
-                <div className="bounds course--detail">
-                    <h1>Create Course</h1>
-                    <div>
-                    <div>
-                        <form>
-                            <div className="grid-66">
-                            <div className="course--header">
-                            <h4 className="course--label">Course</h4>
-                                <div><input id="title" name="title" type="text" className="input-title course--title--input" placeholder="Course title..." defaultValue /></div>
-                                    <p>By Joe Smith</p>
-                                </div>
-                            <div className="course--description">
-                                <div><textarea id="description" name="description" className placeholder="Course description..." defaultValue={""} /></div>
-                            </div>
-                            </div>
-                            <div className="grid-25 grid-right">
-                            <div className="course--stats">
-                                <ul className="course--stats--list">
-                                    <li className="course--stats--list--item">
-                                        <h4>Estimated Time</h4>
-                                        <div><input id="estimatedTime" name="estimatedTime" type="text" className="course--time--input" placeholder="Hours" defaultValue /></div>
-                                    </li>
-                                    <li className="course--stats--list--item">
-                                        <h4>Materials Needed</h4>
-                                        <div><textarea id="materialsNeeded" name="materialsNeeded" className placeholder="List materials..." defaultValue={""} /></div>
-                                    </li>
-                                </ul>
-                            </div>
-                            </div>
-                            <div className="grid-100 pad-bottom"><button className="button" type="submit">Create Course</button><button className="button button-secondary" onclick="event.preventDefault(); location.href='index.html';">Cancel</button></div>
-                        </form>
+        const userData = JSON.parse(localStorage.getItem('userData'));
+        return (
+        <div className="bounds course--detail">
+            <h1>Create Course</h1>
+            <div>
+            <div>
+                <form>
+                    <div className="grid-66">
+                    <div className="course--header">
+                    <h4 className="course--label">Course</h4>
+                        <div><input id="title" name="title" type="text" className="input-title course--title--input" placeholder="Course title..."  /></div>
+                            <p>By Karen Shea</p>
+                        </div>
+                    <div className="course--description">
+                        <div><textarea id="description" name="description" className placeholder="Course description..." defaultValue={""} /></div>
                     </div>
                     </div>
-                </div>
-              );
-            } // end render()
+                    <div className="grid-25 grid-right">
+                    <div className="course--stats">
+                        <ul className="course--stats--list">
+                            <li className="course--stats--list--item">
+                                <h4>Estimated Time</h4>
+                                <div><input id="estimatedTime" name="estimatedTime" type="text" className="course--time--input" placeholder="Hours"  /></div>
+                            </li>
+                            <li className="course--stats--list--item">
+                                <h4>Materials Needed</h4>
+                                <div><textarea id="materialsNeeded" name="materialsNeeded" className placeholder="List materials..." defaultValue={""} /></div>
+                            </li>
+                        </ul>
+                    </div>
+                    </div>
+                    <div className="grid-100 pad-bottom"><button className="button" type="submit">Create Course</button><button className="button button-secondary"><NavLink to='/'>Cancel</NavLink></button></div>
+                </form>
+            </div>
+            </div>
+        </div>
+        );
+    } // end render()
           
 
     } // end class CreateCourse
